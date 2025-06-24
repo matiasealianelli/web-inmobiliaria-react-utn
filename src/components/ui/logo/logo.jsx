4a9aceaea3a5848f92ce73b86eac792123023cd6
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-06-24 08:33:17 -0300

    Create component logo

---
+++ src/components/ui/logo/logo.jsx
@@ -0,0 +1,14 @@
+import "./logo.css";
+export default function Logo() {
+  return (
+    <div className="conteiner-logo-header">
+      <a href="#" className="conteiner-logo-header">
+        <img
+          src="./assets/img/logoRealEstateFooter.svg"
+          alt="icono de illustracion de un edificio utilizada en el logo"
+        />
+        <h3 className="logo">Inmobiliaria</h3>
+      </a>
+    </div>
+  );
+}
